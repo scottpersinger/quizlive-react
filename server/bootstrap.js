212746062f8898f2be2a79f9d4ee21ce832da7b7
Scott Persinger <scottpersinger@gmail.com>
2015-11-13 09:03:12 -0800

    More questions

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -5,11 +5,18 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost');
 
 var models = require('./models');
 
-fs.readFile('./data/questions.js', 'utf8', function(err, data) {
+fs.readFile(__dirname + '/data/questions.js', function(err, buffer) {
+	var data = buffer.toString('utf8');
 	var qs = eval(data);
-	async.map(qs, function(q, cb) {
-		models.Question.create({query:q.question, answers:q.answers, correct_answer:q.answers[q.answer_index-1]}, cb);
-	}, function(err, results) {
-		mongoose.connection.close();
+	models.Question.remove({}).then(function() {
+		async.map(qs, function(q, cb) {
+			console.log(q);
+			models.Question.create({query:q.question, answers:q.answers, correct_answer:q.answers[q.answer_index-1]}, cb);
+		}, function(err, results) {
+			models.Question.count({}).then(function(c) {
+				console.log(c, " total questions");
+				mongoose.connection.close();
+			});
+		});
 	});
 });
